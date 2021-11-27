dfe641520ac9862014c30a38bd866558808cacb9
neigeline-rivollat <88484229+neigeline-rivollat@users.noreply.github.com>
2021-11-27 10:15:42 +0100

    Added comments

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 from flask import request
 
+
+# Creae an app instance
 app = Flask(__name__)
+
+# Connect to a database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///essai.db'
 db = SQLAlchemy(app)
 
 
+# Creating & maintain the database
 class Depense(db.Model):
+    # Defines the column of the db
     id = db.Column(db.Integer, primary_key=True, )
     amount = db.Column(db.Integer, nullable=False)
     categorie = db.Column(db.String(50), nullable=False)
     date = db.Column(db.String(50), nullable=False)
 
+    #  ?
     def __init__(self, amount=None, categorie=None, date=None):
         self.amount = amount
         self.categorie = categorie
@@ -25,10 +32,15 @@ class Depense(db.Model):
     def __repr__(self):
         return '<Depense %r>' % self.id
 
+
+# What happens at the end point
 @app.route('/')
 def hello():
     print("Nice try")
     return render_template('home.html')
+
+#  Time to make requests
+
 
 @app.route("/postdata", methods=['POST'])
 def postdata():
@@ -38,17 +50,24 @@ def postdata():
     db.session.commit()
     return make_response("OK")
 
+# And to show what will be displayed after?
+
+
 @app.route("/seedb")
 def seedb():
     depenses = Depense.query.all()
     deps = []
     for depense in depenses:
-        print("Amount = " + str(depense.amount) + " categorie = " + str(depense.categorie) + " Date = " + str(depense.date))
-        dep = {"amount": depense.amount, "categorie": depense.categorie, "date": depense.date}
+        print("Amount = " + str(depense.amount) + " categorie = " +
+              str(depense.categorie) + " Date = " + str(depense.date))
+        dep = {"amount": depense.amount,
+               "categorie": depense.categorie, "date": depense.date}
         deps.append(dep)
     print(deps)
     return render_template("seedb.html", depenses=deps)
 
 
+# on running python main.py, run the flask app
 if __name__ == '__main__':
+    # Debug mode as long as we are in production mode.
     app.run(debug=True)
