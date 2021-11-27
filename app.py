66eeb996882edf5a7fded0b8bdf1ea32f40fbf59
neigeline-rivollat <88484229+neigeline-rivollat@users.noreply.github.com>
2021-11-27 10:02:38 +0100

    added line to run flask

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
+
+
+if __name__ == "__main__":        # on running python app.py
+    app.run()                     # run the flask app
