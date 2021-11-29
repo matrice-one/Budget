5d59fda4d5988b40b31a9572ecba268c2aed9356
Quentin <Quentin.Rivollat@etu.unige.ch>
2021-11-29 19:06:41 +0100

    Update

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def postdata():
     newdep = Depense(valeur['montant'], valeur['categorie'], valeur['date'])
     db.session.add(newdep)
     db.session.commit()
-    return make_response("OK")
+    return make_response("OK", 200)
 
 @app.route("/seedb")
 def seedb():
@@ -44,10 +44,26 @@ def seedb():
     deps = []
     for depense in depenses:
         print("Amount = " + str(depense.amount) + " categorie = " + str(depense.categorie) + " Date = " + str(depense.date))
-        dep = {"amount": depense.amount, "categorie": depense.categorie, "date": depense.date}
+        dep = {"id" : depense.id, "amount": depense.amount, "categorie": depense.categorie, "date": depense.date}
         deps.append(dep)
     print(deps)
     return render_template("seedb.html", depenses=deps)
+
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    to_delete = request.json
+    print("A supprimer : ", to_delete)
+    for index in to_delete['todelete']:
+        depense = Depense.query.filter_by(id = index).first()
+        db.session.delete(depense)
+        db.session.commit()
+    return make_response("OK", 200)
+
+@app.route("/truc", methods=[])
+def truc():
+    print(request.form)
+    return make_response("OK", 200)
 
 
 if __name__ == '__main__':
